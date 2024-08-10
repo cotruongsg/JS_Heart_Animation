c524f9fa198013a9604a59b80960e8ac9027080b
cotruongsg <cotruongsg@gmail.com>
2024-08-10 16:28:01 -0700

    Fix issue

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const bodyEl = document.querySelector("body");
 
-const createHeart = (event) => {
-  const xPos = event.clientX || event.touches[0].clientX;
-  const yPos = event.clientY || event.touches[0].clientY;
+const createHeart = (x, y) => {
   const spanEl = document.createElement("span");
-  spanEl.style.left = xPos + "px";
-  spanEl.style.top = yPos + "px";
+  spanEl.style.left = x + "px";
+  spanEl.style.top = y + "px";
   const size = Math.random() * 100;
   spanEl.style.width = size + "px";
   spanEl.style.height = size + "px";
@@ -15,13 +13,35 @@ const createHeart = (event) => {
   }, 3000);
 };
 
-// Mouse event
-bodyEl.addEventListener("mousemove", createHeart);
+// Handle mouse events
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.clientX;
+  const yPos = event.clientY;
+  createHeart(xPos, yPos);
+});
 
-// Touch event
+// Handle touch events
+let isTouching = false;
+
 bodyEl.addEventListener("touchstart", (event) => {
   event.preventDefault(); // Prevents scrolling on touch devices
+  isTouching = true;
   for (let i = 0; i < event.touches.length; i++) {
-    createHeart(event);
+    const touch = event.touches[i];
+    createHeart(touch.clientX, touch.clientY);
   }
-}, { passive: false });
+});
+
+bodyEl.addEventListener("touchmove", (event) => {
+  event.preventDefault(); // Prevents scrolling on touch devices
+  if (isTouching) {
+    for (let i = 0; i < event.touches.length; i++) {
+      const touch = event.touches[i];
+      createHeart(touch.clientX, touch.clientY);
+    }
+  }
+});
+
+bodyEl.addEventListener("touchend", () => {
+  isTouching = false;
+});
